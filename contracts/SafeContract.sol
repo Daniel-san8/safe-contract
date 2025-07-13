ddf37d53f5c884b9f78e0670284940bf5fb2339c
Daniel Brígida <danielbrigida@MacBook-Air-de-Daniel.local>
2025-07-13 16:59:54 -0300

    feat: create date to open contract and owner

--- contracts/SafeContract.sol
+++ contracts/SafeContract.sol
@@ -5,18 +5,23 @@ contract SafeContract {
     uint256 public initialSuplyOwner = 100_000;
     string public nameOwner = "Baldwin";
     OwnerStruct public owner;
+    uint256 public dayOpenContract;
 
     struct OwnerStruct {
         address ownerAddress;
         uint256 initialSupply;
         string nameOwner;
+        uint256 createOwner;
     }
 
     constructor () {
         owner = OwnerStruct({
             ownerAddress: msg.sender,
             initialSupply: initialSuplyOwner,
-            nameOwner: nameOwner
+            nameOwner: nameOwner,
+            createOwner: block.timestamp
         });
+
+        dayOpenContract = block.timestamp;
     }
 }
