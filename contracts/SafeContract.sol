93aef95253cedf03cac8df9c409ce05f46d16730
Daniel Brígida <danielbrigida@MacBook-Air-de-Daniel.local>
2025-07-13 16:55:14 -0300

    feat: create owner

--- contracts/SafeContract.sol
+++ contracts/SafeContract.sol
@@ -1,3 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+contract SafeContract {
+    uint256 public initialSuplyOwner = 100_000;
+    string public nameOwner = "Baldwin";
+    OwnerStruct public owner;
+
+    struct OwnerStruct {
+        address ownerAddress;
+        uint256 initialSupply;
+        string nameOwner;
+    }
+
+    constructor () {
+        owner = OwnerStruct({
+            ownerAddress: msg.sender,
+            initialSupply: initialSuplyOwner,
+            nameOwner: nameOwner
+        });
+    }
+}
